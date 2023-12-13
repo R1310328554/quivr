27a05634201bcf0df0f1fd914cbb1cf4a3e20bd8
luok <1310328554@qq.com>
2023-12-13 19:54:11 +0800

    sync fork 好像没有对某些文件进行正确的merge ，导致上游仓库的commit 丢失。。。

--- backend/modules/chat/controller/chat_routes.py
+++ backend/modules/chat/controller/chat_routes.py
@@ -4,37 +4,28 @@ from venv import logger
 
 from fastapi import APIRouter, Depends, HTTPException, Query, Request
 from fastapi.responses import StreamingResponse
-from llm.qa_base import QABaseBrainPicking
-from llm.qa_headless import HeadlessQA
 from middlewares.auth import AuthBearer, get_current_user
-from models import Chat, ChatQuestion, UserUsage, get_supabase_db
-from models.databases.supabase.chats import QuestionAndAnswer
-from modules.user.entity.user_identity import UserIdentity
-from repository.brain.get_brain_by_id import get_brain_by_id
-from repository.chat import (
+from models.user_usage import UserUsage
+from modules.brain.service.brain_service import BrainService
+from modules.chat.controller.chat.factory import get_chat_strategy
+from modules.chat.controller.chat.utils import NullableUUID, check_user_requests_limit
+from modules.chat.dto.chats import ChatItem, ChatQuestion
+from modules.chat.dto.inputs import (
     ChatUpdatableProperties,
     CreateChatProperties,
-    GetChatHistoryOutput,
-    create_chat,
-    get_chat_by_id,
-    get_user_chats,
-    update_chat,
+    QuestionAndAnswer,
 )
-from repository.chat.add_question_and_answer import add_question_and_answer
-from repository.chat.get_chat_history_with_notifications import (
-    ChatItem,
-    get_chat_history_with_notifications,
-)
-from repository.notification.remove_chat_notifications import remove_chat_notifications
-
-from routes.chat.factory import get_chat_strategy
-from routes.chat.utils import (
-    NullableUUID,
-    check_user_requests_limit,
-    delete_chat_from_db,
-)
+from modules.chat.dto.outputs import GetChatHistoryOutput
+from modules.chat.entity.chat import Chat
+from modules.chat.service.chat_service import ChatService
+from modules.notification.service.notification_service import NotificationService
+from modules.user.entity.user_identity import UserIdentity
 
 chat_router = APIRouter()
+
+notification_service = NotificationService()
+brain_service = BrainService()
+chat_service = ChatService()
 
 
 @chat_router.get("/chat/healthz", tags=["Health"])
@@ -54,7 +45,7 @@ async def get_chats(current_user: UserIdentity = Depends(get_current_user)):
     This endpoint retrieves all the chats associated with the current authenticated user. It returns a list of chat objects
     containing the chat ID and chat name for each chat.
     """
-    chats = get_user_chats(str(current_user.id))
+    chats = chat_service.get_user_chats(str(current_user.id))
     return {"chats": chats}
 
 
@@ -66,10 +57,9 @@ async def delete_chat(chat_id: UUID):
     """
     Delete a specific chat by chat ID.
     """
-    supabase_db = get_supabase_db()
-    remove_chat_notifications(chat_id)
+    notification_service.remove_chat_notifications(chat_id)
 
-    delete_chat_from_db(supabase_db=supabase_db, chat_id=chat_id)
+    chat_service.delete_chat_from_db(chat_id)
     return {"message": f"{chat_id}  has been deleted."}
 
 
@@ -81,18 +71,20 @@ async def update_chat_metadata_handler(
     chat_data: ChatUpdatableProperties,
     chat_id: UUID,
     current_user: UserIdentity = Depends(get_current_user),
-) -> Chat:
+):
     """
     Update chat attributes
     """
 
-    chat = get_chat_by_id(chat_id)  # pyright: ignore reportPrivateUsage=none
+    chat = chat_service.get_chat_by_id(
+        chat_id  # pyright: ignore reportPrivateUsage=none
+    )
     if str(current_user.id) != chat.user_id:
         raise HTTPException(
             status_code=403,  # pyright: ignore reportPrivateUsage=none
             detail="You should be the owner of the chat to update it.",  # pyright: ignore reportPrivateUsage=none
         )
-    return update_chat(chat_id=chat_id, chat_data=chat_data)
+    return chat_service.update_chat(chat_id=chat_id, chat_data=chat_data)
 
 
 # create new chat
@@ -105,7 +97,9 @@ async def create_chat_handler(
     Create a new chat with initial chat messages.
     """
 
-    return create_chat(user_id=current_user.id, chat_data=chat_data)
+    return chat_service.create_chat(user_id=current_user.id, chat_data=chat_data)
+
+
 
 
 # add new question to chat
@@ -216,6 +210,7 @@ demoReq = {
 
 
 
+
 # stream new question response from chat
 @chat_router.post(
     "/chat/{chat_id}/question/stream",
@@ -256,7 +251,7 @@ async def create_stream_question_handler(
         fallback_max_tokens = 256
 
         if brain_id:
-            brain = get_brain_by_id(brain_id)
+            brain = brain_service.get_brain_by_id(brain_id)
             if brain:
                 fallback_model = brain.model or fallback_model
                 fallback_temperature = brain.temperature or fallback_temperature
@@ -269,7 +264,6 @@ async def create_stream_question_handler(
     try:
         logger.info(f"Streaming request for {chat_question.model}")
         check_user_requests_limit(current_user)
-        gpt_answer_generator: HeadlessQA | QABaseBrainPicking
         # TODO check if model is in the list of models available for the user
 
         is_model_ok = chat_question.model in user_settings.get("models", ["gpt-3.5-turbo"])  # type: ignore
@@ -301,7 +295,7 @@ async def get_chat_history_handler(
     chat_id: UUID,
 ) -> List[ChatItem]:
     # TODO: RBAC with current_user
-    return get_chat_history_with_notifications(chat_id)
+    return chat_service.get_chat_history_with_notifications(chat_id)
 
 
 @chat_router.post(
@@ -316,4 +310,4 @@ async def add_question_and_answer_handler(
     """
     Add a new question and anwser to the chat.
     """
-    return add_question_and_answer(chat_id, question_and_answer)
+    return chat_service.add_question_and_answer(chat_id, question_and_answer)
